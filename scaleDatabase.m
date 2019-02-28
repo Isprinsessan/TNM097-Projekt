024d0a28f8979b542d543363d05b24f0afebfa88
Bapiro <lucas.rung@gmail.com>
2019-02-28 15:12:26 +0100

    sync function splitimg

---
+++ scaleDatabase.m
@@ -0,0 +1,13 @@
+function [ data_out ] = scaleDatabase( data_in, colors )
+%SCALEDATABASE Summary of this function goes here
+%   data: all images, colors: array with colors
+
+
+
+
+
+
+    
+    
+    
+end
